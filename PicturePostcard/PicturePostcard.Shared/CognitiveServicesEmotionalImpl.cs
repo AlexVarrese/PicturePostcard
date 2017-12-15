7531fabffdd3bb0690ac2fb95f315e919f56eeb3
René Ruppert <rene.ruppert@gmail.com>
2017-12-15 14:52:19 +0100

    fix layout

--- PicturePostcard/PicturePostcard.Shared/CognitiveServicesEmotionalImpl.cs
+++ PicturePostcard/PicturePostcard.Shared/CognitiveServicesEmotionalImpl.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,24 @@ namespace PicturePostcard.Shared
 {
 	public class CognitiveServicesEmotionalImpl : IEmotional
 	{
+		// Get a trial key at https://azure.microsoft.com/en-us/try/cognitive-services/
+		const string API_KEY = "1ecf4c9e-f4ed-45ee-b47a-1227855edbef";
+
+		// Trial keys only work for the West Central US region.
+		const string COGNITIVE_SERVICES_BASE_URL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/";
+
+		// All services are RESTful.
+		readonly HttpClient _client = new HttpClient
+		{
+			BaseAddress = new Uri(COGNITIVE_SERVICES_BASE_URL),
+			Timeout = TimeSpan.FromSeconds(60),
+		};
+
+		public CognitiveServicesEmotionalImpl()
+		{
+			_client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", API_KEY);
+		}
+
 		public async Task<Sentiment> AnalyzeSentimentAsync(string text)
 		{
 			await Task.Delay(500);
@@ -47,13 +67,14 @@ namespace PicturePostcard.Shared
 
 		public async Task<string> RecognizeHandwrittenTextAsync(Stream imageData)
 		{
-			await Task.Delay(500);
+			var requestContent = new StreamContent(imageData);
+			requestContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+			await _client.PostAsync("/recognizeText?handwriting=true", requestContent).ConfigureAwait(false);
 
-			return new[] {
-				"don't worry, be happy",
-				"The funniest people are the saddest ones",
-				"Things could be worse"
-			}[(_index++) % 3];
+			var response = await _client.PostAsync("/vision/v1.0/recognizeText?handwriting=true", requestContent);
+			var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+			return "";
 		}
 
 		public async Task<string> GetImageUrlAsync(string description)
@@ -64,5 +85,4 @@ namespace PicturePostcard.Shared
 
 		int _index;
 	}
-}
 }
